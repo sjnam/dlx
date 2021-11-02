d0f3a9912a37e38182ebdbed8eadc45548307024
sjnam <jsunam@gmail.com>
2021-11-02 15:11:56 +0900

    one line

--- input.go
+++ input.go
@@ -212,16 +212,14 @@ func (d *DLX) inputOptions(line string) error {
 func (d *DLX) InitialContent() {
 	cl, nd := d.cl, d.nd
 	for i := 0; i < d.lastItm; i++ {
-		fmt.Printf("%2d[%2s,%2d,%2d] ",
-			i, cl[i].name, cl[i].prev, cl[i].next)
+		fmt.Printf("%2d[%2s,%2d,%2d] ", i, cl[i].name, cl[i].prev, cl[i].next)
 	}
 	fmt.Println()
 	for i := 0; i <= d.lastNode; i++ {
 		if i%d.lastItm == 0 {
 			fmt.Println()
 		}
-		fmt.Printf("%2d[%2d,%2d,%2d] ",
-			i, nd[i].itm, nd[i].up, nd[i].down)
+		fmt.Printf("%2d[%2d,%2d,%2d] ", i, nd[i].itm, nd[i].up, nd[i].down)
 	}
 	fmt.Println()
 }
