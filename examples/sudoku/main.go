454e965a688fe0678d0168b72dda3cc4c3745749
sjnam <bob.p@klaytn.foundation>
2024-12-07 10:29:01 +0900

    Revert "done channel"
    This reverts commit d8b9ffc60a10736ba00216c62eaf12ae281d3733.

--- examples/sudoku/main.go
+++ examples/sudoku/main.go
@@ -115,18 +115,14 @@ func danceSudoku(line string) [][]byte {
 	return [][]byte{[]byte(line), ans}
 }
 
-func inputLines(ctx context.Context, fd io.Reader) <-chan string {
+func inputLines(fd io.Reader) <-chan string {
 	ch := make(chan string)
 	go func() {
 		defer close(ch)
 
 		scanner := bufio.NewScanner(fd)
 		for scanner.Scan() {
-			select {
-			case <-ctx.Done():
-				return
-			case ch <- strings.TrimSpace(scanner.Text()):
-			}
+			ch <- strings.TrimSpace(scanner.Text())
 		}
 		if err := scanner.Err(); err != nil {
 			log.Fatal(err)
@@ -154,7 +150,7 @@ func main() {
 	defer cancel()
 
 	i := 0
-	for s := range util.OrderedProcess(ctx, inputLines(ctx, fd), danceSudoku) {
+	for s := range util.OrderedProcess(ctx, inputLines(fd), danceSudoku) {
 		i++
 		fmt.Printf("Q[%5d]: %s\n", i, s[0])
 		fmt.Printf("A[%5d]: %s\n", i, s[1])
