7dc26507faae62b3eebdf6ddad151bd3eabdac2f
sjnam <jsunam@gmail.com>
2026-06-07 06:59:37 +0900

    remove io.Pipe

--- examples/sudoku/main.go
+++ examples/sudoku/main.go
@@ -16,88 +16,52 @@ import (
 type puzzle [81]byte
 
 func sudokuDLX(line puzzle) io.Reader {
-	var c, j int
 	var pos, row, col, box [9][9]int
-
-	for i := 0; i < 81; i += 9 {
-		buf := line[i : i+9]
+	for i, j := 0, 0; i < 81; i, j = i+9, j+1 {
 		for k := 0; k < 9; k++ {
-			if buf[k] >= '1' && buf[k] <= '9' {
-				d := int(buf[k] - '1')
+			if ch := line[i+k]; ch >= '1' && ch <= '9' {
+				d := int(ch - '1')
 				x := j/3*3 + k/3
-				pos[j][k] = d + 1
-				if row[j][d] != 0 {
-					log.Fatalf("digit %d appears in columns"+
-						" %d and %d of row %d!", d+1, row[j][d]-1, k, j)
-				}
-				row[j][d] = k + 1
-				if col[k][d] != 0 {
-					log.Fatalf("digit %d appears in rows"+
-						" %d and %d of column %d!", d+1, col[k][d]-1, j, k)
-				}
-				col[k][d] = j + 1
-				if box[x][d] != 0 {
-					log.Fatalf("digit %d appears in rows"+
-						" %d and %d of box %d!", d+1, box[x][d]-1, j, x)
-				}
-				box[x][d] = j + 1
-				c++
+				pos[j][k], row[j][d], col[k][d], box[x][d] = d+1, k+1, j+1, j+1
 			}
 		}
-		j++
 	}
 
-	r, w := io.Pipe()
-	go func() {
-		defer func() {
-			_ = w.Close()
-		}()
-
-		for j = 0; j < 9; j++ {
+	var b bytes.Buffer
+	b.Grow(8192)
+	// Primary items: every unfilled position/row/col/box constraint.
+	for j := 0; j < 9; j++ {
+		for k := 0; k < 9; k++ {
+			if pos[j][k] == 0 {
+				fmt.Fprintf(&b, "p%d%d ", j, k)
+			}
+		}
+	}
+	for _, t := range []struct {
+		c string
+		a *[9][9]int
+	}{{"r", &row}, {"c", &col}, {"b", &box}} {
+		for j := 0; j < 9; j++ {
 			for k := 0; k < 9; k++ {
-				if pos[j][k] == 0 {
-					fmt.Fprintf(w, "p%d%d ", j, k)
+				if t.a[j][k] == 0 {
+					fmt.Fprintf(&b, "%s%d%d ", t.c, j, k+1)
 				}
 			}
 		}
-		for j = 0; j < 9; j++ {
-			for k := 0; k < 9; k++ {
-				if row[j][k] == 0 {
-					fmt.Fprintf(w, "r%d%d ", j, k+1)
+	}
+	b.WriteByte('\n')
+	// One option per legal (position, digit) placement.
+	for j := 0; j < 9; j++ {
+		for k := 0; k < 9; k++ {
+			for d := 0; d < 9; d++ {
+				x := j/3*3 + k/3
+				if pos[j][k] == 0 && row[j][d] == 0 && col[k][d] == 0 && box[x][d] == 0 {
+					fmt.Fprintf(&b, "p%d%d r%d%d c%d%d b%d%d\n", j, k, j, d+1, k, d+1, x, d+1)
 				}
 			}
 		}
-		for j = 0; j < 9; j++ {
-			for k := 0; k < 9; k++ {
-				if col[j][k] == 0 {
-					fmt.Fprintf(w, "c%d%d ", j, k+1)
-				}
-			}
-		}
-		for j = 0; j < 9; j++ {
-			for k := 0; k < 9; k++ {
-				if box[j][k] == 0 {
-					fmt.Fprintf(w, "b%d%d ", j, k+1)
-				}
-			}
-		}
-		fmt.Fprintln(w)
-
-		for j = 0; j < 9; j++ {
-			for k := 0; k < 9; k++ {
-				for d := 0; d < 9; d++ {
-					x := j/3*3 + k/3
-					if pos[j][k] == 0 && row[j][d] == 0 &&
-						col[k][d] == 0 && box[x][d] == 0 {
-						fmt.Fprintf(w, "p%d%d r%d%d c%d%d b%d%d\n",
-							j, k, j, d+1, k, d+1, x, d+1)
-					}
-				}
-			}
-		}
-	}()
-
-	return r
+	}
+	return bytes.NewReader(b.Bytes())
 }
 
 func main() {
